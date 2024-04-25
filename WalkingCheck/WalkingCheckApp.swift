0211d3496ea5aa30bd693b22618e00738cbd2002
exchange-wata <exchange.wata0907@gmail.com>
2024-04-25 16:25:56 +0900

    Initial Commit

---
+++ WalkingCheck/WalkingCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkingCheckApp.swift
+//  WalkingCheck
+//
+//  Created by 渡辺瑞樹 on 2024/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WalkingCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
